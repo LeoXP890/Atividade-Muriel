1dbd81e1d413b30335ce1ee91ef731eac2a3d501
Leonardo Novi <92924990+LeoXP890@users.noreply.github.com>
2022-06-08 18:14:39 -0300

    Add files via upload

---
+++ Trabalho Muriel/Jogador/jogador.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct 
+{
+    int id; //identificação
+    char nome[50];
+    char posicao[50];
+    float altura;
+    int idade;
+    int idTIME; //identificação time
+}Jogador;
+
+
+char cadastro(){
+
+}
