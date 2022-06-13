bc0eb5ad64ea808abd5ddb4634755e57db7624ec
Leonardo Novi <92924990+LeoXP890@users.noreply.github.com>
2022-06-13 15:55:15 -0300

    Add files via upload

---
+++ Atividade-Muriel-main/Trabalho Muriel/Main/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PRETO "\e[0;30m"
+#define VERMELHO "\e[0;31m"
+#define VERDE "\e[0;32m"
+#define AMARELO "\e[0;33m"
+#define AZUL "\e[0;34m"
+#define MAGENTA "\e[0;35m"
+#define CYANO "\e[0;36m"
+#define BRANCO "\e[0;37m"
+#define reset "\e[0m" // VOLTA A COR ORIGINAL
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
